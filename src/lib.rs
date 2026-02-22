97e5d074d2363e9ccd2a93122827b7a49fde84be
wooker <zakhar.semyonov@gmail.com>
2026-02-22 23:07:23 +0500

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,86 @@ pub mod packet;
 pub mod router;
 pub mod transport;
 pub mod types;
+
+#[allow(unused_imports)]
+mod tests {
+    use crate::{
+        constants::PROTOCOL_VERSION,
+        error::EncodeError,
+        message::Message::Invoke,
+        packet::{HEADER_LEN, MAX_BUFFER_LENGTH, MAX_PACKET_LENGTH, MAX_PAYLOAD_LENGTH, Packet},
+        types::Flags,
+    };
+
+    #[test]
+    fn encode_empty() {
+        let payload = &[];
+        let packet = Packet {
+            version: PROTOCOL_VERSION,
+            flags: Flags::empty(),
+            packet_id: 0,
+            src: 0,
+            dst: 0,
+            ttl: 0,
+            msg_type: Invoke,
+            payload,
+        };
+        let mut out = [0; MAX_BUFFER_LENGTH];
+        let encoded = packet.encode(&mut out);
+        assert_eq!(encoded.is_ok(), true);
+        let size = encoded.unwrap();
+        let mut in_buf = [0; MAX_BUFFER_LENGTH];
+        let decoded = Packet::decode(&out[..size], &mut in_buf);
+        assert_eq!(decoded.is_ok(), true);
+        let decoded = decoded.unwrap();
+        assert_eq!(packet, decoded);
+    }
+
+    #[test]
+    fn encode_decode_full_unescaped() {
+        let payload = &[0; MAX_PAYLOAD_LENGTH];
+        let packet = Packet {
+            version: PROTOCOL_VERSION,
+            flags: Flags::empty(),
+            packet_id: 0,
+            src: 0,
+            dst: 0,
+            ttl: 0,
+            msg_type: Invoke,
+            payload,
+        };
+        let mut out = [0; MAX_BUFFER_LENGTH];
+        let encoded = packet.encode(&mut out);
+        assert_eq!(encoded.is_ok(), true);
+        let size = encoded.unwrap();
+        let mut in_buf = [0; MAX_BUFFER_LENGTH];
+        let decoded = Packet::decode(&out[..size], &mut in_buf);
+        assert_eq!(decoded.is_ok(), true);
+        let decoded = decoded.unwrap();
+        assert_eq!(packet, decoded);
+    }
+
+    #[test]
+    fn encode_decode_full_escaped() {
+        let payload = &[0x7f; MAX_PAYLOAD_LENGTH];
+        let packet = Packet {
+            version: PROTOCOL_VERSION,
+            flags: Flags::empty(),
+            packet_id: 0,
+            src: 0,
+            dst: 0,
+            ttl: 0,
+            msg_type: Invoke,
+            payload,
+        };
+        let mut out = [0; MAX_BUFFER_LENGTH];
+        let encoded = packet.encode(&mut out);
+        assert_eq!(encoded.is_ok(), true);
+        let size = encoded.unwrap();
+        let mut in_buf = [0; MAX_BUFFER_LENGTH];
+        let decoded = Packet::decode(&out[..size], &mut in_buf);
+        assert_eq!(decoded.is_ok(), true);
+        let decoded = decoded.unwrap();
+        assert_eq!(packet, decoded);
+    }
+}
